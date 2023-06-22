22fbdf1ce42688d3f47a597c6ddba77cecfc8676
nnhoang215 <nnhoang215@gmail.com>
2023-06-22 12:06:46 +1000

    setup route master & riverpod

---
+++ lib/app/app.dart
@@ -0,0 +1,25 @@
+export 'utils.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_docs_clone/app/navigation/routes.dart';
+import 'package:routemaster/routemaster.dart';
+
+class GoogleDocsApp extends ConsumerStatefulWidget {
+  const GoogleDocsApp({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _GoogleDocsAppState();
+}
+
+class _GoogleDocsAppState extends ConsumerState<GoogleDocsApp> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerDelegate: RoutemasterDelegate(routesBuilder: (context) {
+        return routesLoggedOut;
+      }), 
+      routeInformationParser: const RoutemasterParser()
+    );
+  }
+}
